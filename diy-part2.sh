6d6f525e53779b4b60e7975046d3d85c76a6876c
zgs360 <zgs360@gmail.com>
2025-01-21 18:51:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-#============================================================
-# https://github.com/Lancenas/Actions-Lean-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+#=================================================
+# Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#2. Clear the login password
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Modify default Hostname
+sed -i 's/ImmortalWrt/k2p/g' package/base-files/files/bin/config_generate
+
+#4. 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
