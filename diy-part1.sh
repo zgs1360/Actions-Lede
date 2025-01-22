5c4c9c00882ca1a01131f02bee93bb155c2bb4b4
zgs360 <zgs360@gmail.com>
2025-01-22 19:54:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,14 +9,13 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# use ssr
 sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
 sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
 
-# Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# Enable helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# kenzok8/openwrt-package
-# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# Add passwall
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
