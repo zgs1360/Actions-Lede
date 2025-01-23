df45eea4f078856f5cadc8cf743513280ebdeb7f
zgs360 <zgs360@gmail.com>
2025-01-23 20:56:45 +0800

    Update diy_x86.sh

--- diy_x86.sh
+++ diy_x86.sh
@@ -11,9 +11,7 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
+
+# 选用ssr要更改
+sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
+sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
