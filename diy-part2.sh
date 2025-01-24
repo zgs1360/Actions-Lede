87c3b3fe1f16acf394ce3e958c2b436d451f2736
zgs360 <zgs360@gmail.com>
2025-01-24 19:07:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,9 @@ sed -i 's/192.168.1.1/192.168.2.123/g' package/base-files/files/bin/config_gener
 #2. 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
+#3. 修改时间格式  
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm  
+  
+#4. 添加固件日期  
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk  
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk  
