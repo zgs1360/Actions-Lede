f3d472a6993cf0db2f5392da9e107a03161987bd
zgs360 <zgs360@gmail.com>
2025-01-24 14:12:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,14 @@
 #=============================================================
 
 # use ssr
-sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
-sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
+#sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
+#sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
 
 # Enable helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add passwall
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
