65c987a8ddc8e392f5102014e84eb735da90ae83
zgs360 <zgs360@gmail.com>
2025-01-24 19:06:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,10 @@ sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
 # Enable helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add passwall
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+# 添加 PassWall 插件  
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default  
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default  
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default  
 
 # Add ddn-go
 echo 'src-git ddns-go https://github.com/sirpdboy/luci-app-ddns-go.git;main' >> feeds.conf.default
