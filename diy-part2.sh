bae488d1546baa730c00d16997911bcd62d6f2e4
zgs360 <zgs360@gmail.com>
2025-01-25 11:08:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,18 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.123/g' package/base-files/files/bin/config_generate
 
-#2. 最大连接数修改为65535
+# 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
-#3. 修改时间格式  
+# 修改时间格式  
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm  
   
-#4. 添加固件日期  
+# 添加固件日期  
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk  
 sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk  
+
+# 修改默认主机名为MiyaWrt
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='ZgsWrt'' package/lean/default-settings/files/zzz-default-settings
+
+# 加入编译者信息
+sed -i "s/OpenWrt /Zgs360 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
