e4c1458bcb64f2b0a3ca9c8884c85089b4d4e536
zgs1360 <zgs1360@gmail.com>
2025-01-26 08:24:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,18 +7,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-
-# use ssr
-sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
-sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
-
 # Enable helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加 PassWall 插件  
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default  
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default  
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default  
-
-# Add ddn-go
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+# Add istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
