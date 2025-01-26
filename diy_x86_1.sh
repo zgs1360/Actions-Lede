cdb91b5126a4e3c784a688f3180335e4e96ded27
zgs1360 <zgs1360@gmail.com>
2025-01-26 17:55:15 +0800

    Create diy_x86_1.sh

---
+++ diy_x86_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+
+
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
