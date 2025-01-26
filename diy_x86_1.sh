498a9888aebc757e93817b01fe208dda19d0b471
zgs1360 <zgs1360@gmail.com>
2025-01-26 20:44:26 +0800

    Update diy_x86_1.sh

--- diy_x86_1.sh
+++ diy_x86_1.sh
@@ -7,8 +7,22 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
+# use ssr
+sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
+sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
+
+# Enable helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 添加 PassWall 插件  
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default  
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default  
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
+
+# Add istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 
-
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# Add ddns_to
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
