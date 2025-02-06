d549fbae9737752014d67c42a19632519a934fdc
zgs1360 <zgs1360@gmail.com>
2025-02-06 19:53:09 +0800

    Update diy_x86_1.sh

--- diy_x86_1.sh
+++ diy_x86_1.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# use ssr
-sed -i 's/^#\(.*coolsnowwolf\/luci\)/\1/' feeds.conf.default
-sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
-
-# Enable helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 添加 PassWall 插件  
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default  
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default  
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
-
-# Add istore
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# 添加 kenzo 插件  
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
