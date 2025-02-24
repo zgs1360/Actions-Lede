c75adb8b5a56a938772ecab7312cc5e0ee3f3317
zgs1360 <zgs1360@gmail.com>
2025-02-24 17:16:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ sed -i 's/^[^#]\(.*openwrt-23\.05\)/#&/g' feeds.conf.default
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add passwall
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default  
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default  
 
 
