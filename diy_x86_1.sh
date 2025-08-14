696e4f64161fffff629a3775d5a94a2ea4da7d52
zgs1360 <zgs1360@gmail.com>
2025-08-14 22:46:17 +0800

    Update diy_x86_1.sh

--- diy_x86_1.sh
+++ diy_x86_1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 添加 kenzo 插件  
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
